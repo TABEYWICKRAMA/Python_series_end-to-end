2fe040f9a02b5f953373bd5c13faf8f3ee431b47
TABEYWICKRAMA <110693737+TABEYWICKRAMA@users.noreply.github.com>
2022-08-10 10:45:23 +0530

    Python_While_Loop_V1

---
+++ while loop.py
@@ -0,0 +1,140 @@
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+#output
+
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+
+
+
+
+
+i = 1
+while i <= 10:
+    print(i * '*')
+    i += 1
+
+#output
+
+# *
+# **
+# ***
+# ****
+# *****
+# ******
+# *******
+# ********
+# *********
+# **********
+
+
+
+
+
+#this is copy to for loop
+# i = 1
+# for i in range(3):
+#     for j in range(3):
+#         print(i*'*')
+
+# i=1
+# while i<=3:
+#     print(i)
+#This will generate a loop without stopping.
+
+i=1
+while i<=3:
+    print(i)
+    i+=1
+# 1
+# 2
+# 3
+
+
+
+#i -> rows
+#j -> columns
+
+num = int(input('Please enter any Side of a Square: '))
+print('Square Star Pattern')
+
+i = 0
+while i <= num:
+    j = 0
+    while j <= num:
+        j += 1
+        print('*')
+
+    i += 1
+    print('')
+
+# *
+# *
+# *
+#
+# *
+# *
+# *
+#
+# *
+# *
+# *
+
+num = int(input('Please enter any Side of a Square: '))
+print('Square Star Pattern')
+
+i = 0
+
+while i <= num:
+    j = 0
+    while j <= num:
+        j += 1
+        print('*', end=' ')    #This print statement append all stars
+    i += 1
+    print('')   #This print statement control cursor to new line
+
+# * * *
+# * * *
+# * * *
+
+
+# print('*', end=' ')
+# The end parameter of print()function is used to
+# set the value of the string to be appended at the end of the print() function.
+# By default it's value is set to the newline character \n unless specified otherwise.
+
+
+
+#using \n in last print statement
+#then there is a line between stars
+
+num = int(input('Please enter any Side of a Square: '))
+print('Square Star Pattern')
+
+i = 0
+
+while i <= num:
+    j = 0
+    while j <= num:
+        j += 1
+        print('*', end=' ')
+
+    i += 1
+    print('\n')
+
+# * * *
+#
+# * * *
+#
+# * * *
